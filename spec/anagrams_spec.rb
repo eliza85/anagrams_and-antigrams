423086a9838318534db8cc39e52a216856428420
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-13 11:12:13 -0800

    create spec checking if input is actual word

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -27,13 +27,13 @@ describe("Anagram#anagram?") do
     test1 = Test.new("Eat", "Tea")
     expect(test1.anagram?()).to(eq(true))
   end
+  it ("An input that is not a word will return an error message") do
+    test1 = Test.new("Eat", "Tzrybq")
+    expect(test1.anagram?()).to(eq("You need to input actual words!"))
+  end
 end
 
 
-# Check if two words are anagrams. If they are, your method should return something like this: "These words are anagrams."
-#
-# Account for the possibility that words might have different cases but should still be anagrams. For instance, "Tea" is still an anagram of "Eat".
-#
 # Add a rule to check if the inputs are words. Words can only be anagrams or anagrams if they are, in fact, actually words. (For instance, "yurb" isn't an anagram of "ruby" because "yurb" isn't a real word.) A word must contain a vowel (a, i, e, o, u) or y. Otherwise it's not a word. If one of the inputs has no vowels, your method should return something like this: "You need to input actual words!" (You only need to write a rule for vowels.)
 #
 # If phrases aren't anagrams, the method should check whether they are actually "antigrams." For our purposes, a word is an "antigram" of another word if no letters match. For example, "hi" has no matches with "bye". The method should return something like this if there are no letter matches: "These words have no letter matches and are antigrams."
