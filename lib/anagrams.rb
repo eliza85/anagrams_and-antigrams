2977ef5b570c1ef89bb9c052a3db31925e4d546b
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-13 13:23:56 -0800

    refactor anagram? method to include isword method inside

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,39 +1,22 @@
-# PLUMBED WITHOUT INSTANCE VARIABLES
-# class Dude
-#
-#   def function_dude (input1, input2)
-#     result = input1 + input2
-#     self.function_tight(result)
-#   end
-#
-#   def function_tight(result)
-#     super_array = [result, result, result]
-#     super_array.reduce(:+)
-#   end
-# end
-
-
-
-
-
-# PLUMBED WITH INSTANCES
 class Test
 
   def initialize(input1, input2)
     @input1 = input1.downcase.split("")
     @input2 = input2.downcase.split("")
-    # @vowels = ["a", "e", "i", "o", "u", "y"]
   end
 
   def anagram?
     if
-    @input1.sort == @input2.sort
-    return "These words are anagrams!"
-  else
-    return "These words are not anagrams!"
-  end
-    # result = @input1 + @input2
-    # self.function_tight(result)
+    self.isword?
+      if
+        @input1.sort == @input2.sort
+        return "These words are anagrams!"
+      else
+        return "These words are not anagrams!"
+      end
+    else
+    return "You need to input actual words!"
+    end
   end
 
   def isword?
@@ -43,14 +26,13 @@ class Test
     @input1.any? {|input1| vowels.include?(input1)}
       if
       @input2.any? {|input2| vowels.include?(input2)}
-      return true
+        return true
+      else
+        return "You need to input actual words!"
+      end
     else
       return "You need to input actual words!"
     end
-  else
-    return "You need to input actual words!"
-  end
-
   end
 
     # >> cheeses = %w(chedder stilton brie mozzarella feta haloumi)
